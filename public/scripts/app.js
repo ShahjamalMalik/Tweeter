46ec3cef44a8e290b3407387650f050187a94451
ShahjamalMalik <shahjamalmalik@gmail.com>
2019-01-23 21:28:39 +0000

    Added Ajax requests to fetch and post tweets, Added error messages for specific conditions, Tweets are now posted without a refresh, Added an escape function to protect from XSS, Created a form toggle.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,74 +1,16 @@
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 function renderTweets(tweets) {
   for (let id of tweets) {
-    $("#tweets").append(createTweetElement(id));
+    $("#tweets").prepend(createTweetElement(id));
   }
 }
 
-// This object takes the html from the index.html in string format and applies key:value pairs from the obj tweetData to variable article
-// It returns the article
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+
 function createTweetElement(obj) {
   let article = `
   <article class="tweet">
@@ -79,7 +21,7 @@ function createTweetElement(obj) {
         </header>
         <div class="tweet-content">
           <p>
-            ${obj.content.text}
+            ${escape(obj.content.text)}
           </p>
         <footer>
           <p id="yo">
@@ -92,16 +34,59 @@ function createTweetElement(obj) {
         </div>
        </footer>
    </article>
-`
-  let $tweet = $(article)
+`;
+  let $tweet = $(article);
   return $tweet;
 }
 
-// contains a variable tweet which is a holder for the callback function createTweetElement
-// that is appended to the .main-container css
 $(document).ready(function() {
 
+  let loadTweets = function() {
+    $('#tweets').empty();
+    $.ajax("/tweets", {method: 'GET'})
+    .then(function (tweets) {
+      renderTweets(tweets);
+    });
+  };
+  loadTweets();
 
 
-renderTweets(data);
+  var $submitTweet = $('.submitTweet');
+
+  $submitTweet.submit(function (event) {
+    event.preventDefault();
+    let $tweetContent = $(this);
+    let submitable = checkTweet();
+
+    if (submitable) {
+      $.ajax('/tweets', {
+        method: 'POST',
+        data: $tweetContent.serialize()
+      })
+      .done(function() {
+        loadTweets();
+      });
+    }
+  });
+
+  const checkTweet = function () {
+    let checkTextArea = $('.textArea').val();
+
+    if (checkTextArea === '' || checkTextArea === null) {
+      $('.errorBlank').removeClass("hidden");
+      return false;
+
+    } else if (checkTextArea.length > 140) {
+      $('.error140').removeClass("hidden");
+        return false;
+    } else {
+        return true;
+      }
+  };
+
+ $(".new-tweet").hide();
+ $('.large-button').click(function() {
+   $('.new-tweet').slideToggle('fast');
+ });
 });
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 
-  $("textarea").on("keypress", function(e) {
-
+  $("textarea").on("keyup", function(e) {
+    $('.errorBlank').addClass("hidden");
 
     if ($("textarea").val().length > 140) {
       $(".counter").css('color', 'red');
     }
     else {
       $(".counter").css('color', 'black');
-      }
+      $('.error140').addClass("hidden");
+    }
 
     $('.counter').text(140 - $(this).val().length);
 
