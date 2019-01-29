7ccb0a0e475264aaed7557251391efa569ad8e44
Shahjamal Malik <43356252+ShahjamalMalik@users.noreply.github.com>
2019-01-29 12:23:23 -0500

    Added comments, fixed lint errors.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+//Function so tweet displays
 function renderTweets(tweets) {
   for (let id of tweets) {
     $("#tweets").prepend(createTweetElement(id));
@@ -5,34 +6,34 @@ function renderTweets(tweets) {
 }
 
 function escape(str) {
-  var div = document.createElement('div');
+  const div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
-
+//Function to create tweet
 function createTweetElement(obj) {
   let article = `
   <article class="tweet">
-        <header>
-          <img src=${obj.user.avatars.small}>
-          <h1>${obj.user.name}</h1>
-          <h2>${obj.user.handle}</h2>
-        </header>
-        <div class="tweet-content">
-          <p>
-            ${escape(obj.content.text)}
-          </p>
-        <footer>
-          <p id="yo">
-            ${obj.created_at}
-          </p>
-          <div class="animation">
-            <i class="fa fa-flag" aria-hidden="true"></i>
-            <i class="fa fa-retweet" aria-hidden="true"></i>
-            <i class="fa fa-heart" aria-hidden="true"></i>
-        </div>
-       </footer>
+    <header>
+      <img src=${obj.user.avatars.small}>
+      <h1>${obj.user.name}</h1>
+      <h2>${obj.user.handle}</h2>
+    </header>
+    <div class="tweet-content">
+      <p>
+        ${escape(obj.content.text)}
+      </p>
+      <footer>
+        <p id="yo">
+          ${obj.created_at}
+        </p>
+        <div class="animation">
+          <i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+      </div>
+      </footer>
    </article>
 `;
   let $tweet = $(article);
@@ -42,8 +43,8 @@ function createTweetElement(obj) {
 $(document).ready(function() {
 
   let loadTweets = function() {
-    $('#tweets').empty();
-    $.ajax("/tweets", {method: 'GET'})
+    $("#tweets").empty();
+    $.ajax("/tweets", {method: "GET"})
     .then(function (tweets) {
       renderTweets(tweets);
     });
@@ -51,16 +52,16 @@ $(document).ready(function() {
   loadTweets();
 
 
-  var $submitTweet = $('.submitTweet');
+  var $submitTweet = $(".submitTweet");
 
   $submitTweet.submit(function (event) {
     event.preventDefault();
     let $tweetContent = $(this);
-    let submitable = checkTweet();
+    const submitable = checkTweet();
 
     if (submitable) {
-      $.ajax('/tweets', {
-        method: 'POST',
+      $.ajax("/tweets", {
+        method: "POST",
         data: $tweetContent.serialize()
       })
       .done(function() {
@@ -68,25 +69,24 @@ $(document).ready(function() {
       });
     }
   });
-
+//Function to check if tweet is valid
   const checkTweet = function () {
-    let checkTextArea = $('.textArea').val();
+    let checkTextArea = $(".textArea").val();
 
-    if (checkTextArea === '' || checkTextArea === null) {
-      $('.errorBlank').removeClass("hidden");
+    if (!checkTextArea) {
+      $(".errorBlank").removeClass("hidden");
       return false;
 
     } else if (checkTextArea.length > 140) {
-      $('.error140').removeClass("hidden");
-        return false;
+      $(".error140").removeClass("hidden");
+      return false;
     } else {
-        return true;
+      return true;
       }
   };
-
- $(".new-tweet").hide();
- $('.large-button').click(function() {
-   $('.new-tweet').slideToggle('fast');
+//Toggle for compose button
+  $(".new-tweet").hide();
+  $(".large-button").click(function() {
+    $(".new-tweet").slideToggle("fast");
  });
 });
-
